929f2a48f1c341056cfb56fad65dd0a9d70f537b
Emrul <emrul.hoque@gmail.com>
2023-03-24 09:44:53 +0600

    update

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+class Program
+
+{
+
+    static void Main()
+    {
+        Console.WriteLine("Type a sentence and the program will reverse its word order.");
+        string? sentence = Console.ReadLine();
+
+        //split the sentence into words
+        if (sentence != null)
+        {
+            string[] words = sentence.Split(' ');
+
+            //Reverse the order of the words
+            Array.Reverse(words);
+
+            //Join the words back into a sentence
+            string reversed = string.Join(" ", words);
+
+            Console.WriteLine(reversed);
+
+        }
+
+        else
+        {
+            //do nothing;
+        }
+
+    }
+
+}
